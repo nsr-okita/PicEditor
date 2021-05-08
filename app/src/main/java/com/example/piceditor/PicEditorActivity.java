9f526aa3a820c4e9e9d86ddc3248fe876978a655
nsr-seki <prototype.f91@gmail.com>
2021-05-08 14:48:55 +0900

    画面操作・画面表示対応 【修正内容】 ・「＜(メニューに戻る)」ボタンを押下時ダイアログが表示するように修正 ・編集終了ダイアログの「はい」を押下時はメニュー画面へ遷移 　編集終了ダイアログの「いいえ」を編集画面に戻るように作成 ・編集画面の不要なテキストを削除

--- app/src/main/java/com/example/piceditor/PicEditorActivity.java
+++ app/src/main/java/com/example/piceditor/PicEditorActivity.java
@@ -1,12 +1,13 @@
 package com.example.piceditor;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PicEditorActivity  extends AppCompatActivity {
@@ -20,27 +21,40 @@ public class PicEditorActivity  extends AppCompatActivity {
     //画像編集画面
     private void setScreenEditor(){
         setContentView(R.layout.activity_canvas);
-        String[] test;
-        TextRead file = new TextRead();
-        file.Init(getApplicationContext(),"Last_file_data.txt",2);
 
         ImageView imageView = findViewById(R.id.image_view);
         imageView.setImageResource(R.drawable.img);
-        test = file.readFile();
-        TextView textView2 = findViewById(R.id.text3);
-        textView2.setText(test[0]);
-        file.saveFile("00000,1,2,3,4\n",false);
 
         Button returnButton = findViewById(R.id.return_button);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditEndDialog();
+            }
+        });
+    }
+    private void EditEndDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("この処理で編集内容の保存はされません。\n本当に終了してもよろしいでしょうか？");
+        builder.setPositiveButton("はい", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int idx) {
+                TextRead file = new TextRead();
+                file.Init(getApplicationContext(),"Last_file_data.txt",2);
+                file.saveFile("test.png\n",false);
                 //ボタンタップでスタート画面に遷移
                 Intent intent = new Intent(getApplication(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+        builder.setNegativeButton("いいえ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int idx) {
 
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
